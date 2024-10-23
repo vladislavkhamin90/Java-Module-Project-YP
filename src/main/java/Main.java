3e09916f7ce3da2a4380adf77a03d68a72e3aad6
user <vladislavkhamin90@gmail.com>
2024-10-23 17:38:34 +0300

    Проектная работа №1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,72 @@
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+//класс для т.с.
+class Auto {
+
+    String name; // имя т.с.
+    int speed; // скорость т.с.
+
+    // конструктор
+    Auto(String name, int speed){
+
+        this.name = name;
+        this.speed = speed;
+
     }
 }
+
+// класс для хранения текущего лидера гонок
+class Race{
+
+    String raceLeader = ""; // лидер гонки
+    int distance = 0; // пройденная дистанция
+
+    // функция для расчета дистанции и выявления лидера
+    void findLeader(String nameAuto, int speedAuto){
+
+        if(24 * speedAuto > distance){
+            distance = 24 * speedAuto;
+            raceLeader = nameAuto;
+        }
+
+
+    }
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // переменные для создания объектов
+        Scanner scanner = new Scanner(System.in);
+        Auto auto;
+        Race race = new Race();
+
+        // цикл для создания трех экземпляров класса Auto
+        for(int i = 1; i <= 3; i++ ) {
+
+            System.out.println("Введите название машины №" + i);
+            String name = scanner.next();
+
+            System.out.println("Введите скорость машины №" + i);
+            // вложенный цикл для обработки значения переменной скорости
+            while (true){
+
+                int speed = scanner.nextInt();
+                if (speed > 0 && speed <=250){
+
+                    auto = new Auto(name, speed);
+                    race.findLeader(auto.name,auto.speed);
+                    break;
+
+                }else{
+                    System.out.println("Неправильная скорость");
+                }
+            }
+        }
+        System.out.println("Самая быстрая машина: " + race.raceLeader);
+    }
+}
+
